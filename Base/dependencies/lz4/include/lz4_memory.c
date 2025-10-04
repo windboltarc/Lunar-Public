8f6e0dffe74c6702a8aaf1352cb1c7fd4f4a2e44
BlastHuynz <giaphuocklg2@gmail.com>
2025-10-04 12:16:16 +0700

    Add files via upload

---
+++ Base/dependencies/lz4/include/lz4_memory.c
@@ -0,0 +1,17 @@
+#include "lz4.h"
+#include <stdlib.h>
+
+void* LZ4_malloc(size_t s)
+{
+    return malloc(s);
+}
+
+void* LZ4_calloc(size_t n, size_t s)
+{
+    return calloc(n, s);
+}
+
+void LZ4_free(void* p)
+{
+    free(p);
+}
